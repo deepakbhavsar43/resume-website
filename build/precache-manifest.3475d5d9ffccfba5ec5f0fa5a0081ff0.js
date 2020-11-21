48b2035c2c14be7e8f912a059fbf73c92c4b3081
deepakbhavsar43 <deepakbhavsar43@gmail.com>
2020-11-21 21:07:29 +0530

    Update : resume.json

---
+++ build/precache-manifest.3475d5d9ffccfba5ec5f0fa5a0081ff0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f608e4234158686ca6063ff92d0abd7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbce4dce82e865ffdd0c",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbce4dce82e865ffdd0c",
+    "url": "/static/js/main.9d4f5508.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
